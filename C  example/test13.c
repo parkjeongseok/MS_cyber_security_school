2f0fb48f0c28ceb1f30b8bc7026e87189c8cde27
user <user@Ubuntu-24.04-parkjeongseok>
2025-08-29 11:26:27 +0900

    linux and organize files

--- C  example/test13.c
+++ C  example/test13.c
@@ -1,27 +1,29 @@
 #include<stdio.h>
 
-//char *str = "Good afternoon";	// const ¿µ¾ï 
-char *s1 = "Good";
-char *s2 = "afternoon";	// const ¿µ¾ï 
-char *ss[] = { s1, s2 };
-
-int main(){
-//	char* str = "Good afternoon";	// "¾È¿¡ ¹®ÀåÀº »ó¼ö¸¦ ÀÇ¹ÌÈù´Ù.
-//	»ó¼ö¸¦ º¯°æÇÏ·Á°í Çß±â ¶§¹®¿¡ ¹®Á¦°¡ »ı±ä´Ù. 
-
-//	char str[] = "Good afternoon";
-//	Heap¿µ¿ª¿¡¼­ ¸¸µé°í stack¿µ¿ªÀ¸·Î °¡Áö°í °¡¼­ º¯°æÀ» ÇØÁØ°Å´Ù. 
-	
-//	char b[100];
-//	strcpy(b, str);
-//	char *p = b;
-//	
-//	printf("Before : \"%s\"\n", p);
-//	
-//	*(p+5) = '_';
-//	
-//	printf("After : \"%s\"", p);
-
-	printf("\"%s %s\" \n", s1, s2);
-	printf("\"%s %s\" \n", ss[0], ss[1]);
+char *title[] = {"ë²ˆí˜¸", "ì´ë¦„", "êµ­ì–´", "ì˜ì–´", "ìˆ˜í•™"};
+char *name[] = {"ë°•ì •ì„", "ë°•ì¬ì€", "ë°•ì‹œí˜œ", "ì„ì¤€í˜", "ì´ìŠ¹ë¹ˆ"};
+int kor[] = {10, 20, 30, 40, 50};
+int eng[] = {60, 70, 80, 90, 80};
+int mat[] = {70, 60, 30, 40, 50};
+int main(void){
+//  int i = 10;
+//  int *p = &i;
+//  int **pp = &p;
+  
+//  printf("i  : %08x\t (%08x)\n",          &i,  i);
+//  printf("p  : %08x\t (%08x)\t (%08x)\n", &p,  p,  *p);
+//  printf("pp : %08x\t (%08x)\t (%08x)\n", &pp, pp, **pp);
+  
+  int nameSize = sizeof(name) / sizeof(name[0]);
+//  printf("%d\n", size);
+  for(int i = 0; i < nameSize; i++){
+    if( i == 0 ){
+      for(int j=0; j<nameSize ;j++){
+        printf("%s\t", title[j]);
+      }
+      printf("\n");
+    } else {
+      printf("%2d\t%s\t%3d\t%3d\t%3d\n", i, name[i], kor[i], eng[i], mat[i]);
+    }
+  }
 }
